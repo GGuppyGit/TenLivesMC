2f73f2ac479261d7582adc7d6006de75bf100986
MGNKylan <kylanplunk@gmail.com>
2021-11-13 11:14:11 -0600

    Added a message when trying to set lives higher than 10 Added required permission node for /setlives and usage information

--- src/main/java/com/gguppy/tenlives/Commands.java
+++ src/main/java/com/gguppy/tenlives/Commands.java
@@ -15,25 +15,36 @@ import java.io.IOException;
 public class Commands implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if(command.getName().equalsIgnoreCase("setlives")){
-            if((args.length != 2)){
-                sender.sendMessage(TenLives.colorFormat("&c&l❌ &cUsage: /setlives player amount"));
-            } else {
-                Player p = Bukkit.getPlayer(args[0]);
-                int amount = Integer.parseInt(args[1]);
-                int deaths = 11 - amount;
-                File f = new File(TenLives.getPlugin().getDataFolder().getAbsolutePath() + "/userdata/", p.getUniqueId() + ".yml");
-                FileConfiguration c = YamlConfiguration.loadConfiguration(f);
-                c.set("deaths", deaths);
-                p.setMaxHealth(deaths * 2);
-                sender.sendMessage(TenLives.colorFormat("&a&l✔ " + "&a" + p.getName() + " now has &c" + amount + "&a lives and &c" + (11- amount) + " &ahearts."));
-                try {
-                    c.save(f);
-                } catch (IOException e) {
-                    e.printStackTrace();
+        if (sender.hasPermission("tenlives.manage")) {
+            if (command.getName().equalsIgnoreCase("setlives")) {
+                if ((args.length != 2)) {
+                    sender.sendMessage(TenLives.colorFormat("&c&l❌ &cUsage: /setlives player amount"));
+                } else {
+                    Player p = Bukkit.getPlayer(args[0]);
+                    int amount = Integer.parseInt(args[1]);
+                    int deaths = 11 - amount;
+                    File f = new File(TenLives.getPlugin().getDataFolder().getAbsolutePath() + "/userdata/", p.getUniqueId() + ".yml");
+                    FileConfiguration c = YamlConfiguration.loadConfiguration(f);
+
+                    try {
+                        p.setMaxHealth(deaths * 2);
+                        c.set("deaths", deaths);
+                    } catch (IllegalArgumentException e){
+                        sender.sendMessage(TenLives.colorFormat("&c&l❌ You must use an amount less than 11"));
+                    }
+                    sender.sendMessage(TenLives.colorFormat("&a&l✔ " + "&a" + p.getName() + " now has &c" + amount + "&a lives and &c" + (11 - amount) + " &ahearts."));
+                    try {
+                        c.save(f);
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
                 }
             }
+
+        } else {
+            sender.sendMessage(TenLives.colorFormat("&c&l❌ &cYou do not have access to that command."));
         }
         return true;
     }
+
 }
